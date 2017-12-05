f2231797d986b7420720cddd0b2f36f20247c0ab
xiaohong.dong <xiaohong.dong@htouhui.com>
2017-12-05 15:28:46 +0800

    加入Hql解析;加入一对多配置;一对多配置需要多的一方反转,由一的一方维护关系

--- src/main/java/com/work/hn/model/User.java
+++ src/main/java/com/work/hn/model/User.java
@@ -52,4 +52,14 @@ public class User implements Serializable{
     public void setNamer(Namer namer) {
         this.namer = namer;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", namer=" + namer +
+                '}';
+    }
 }
--- src/test/java/HibernateTest.java
+++ src/test/java/HibernateTest.java
@@ -48,4 +48,36 @@ public class HibernateTest {
         String sql = ParseHqlHelper.holderParamsParser(hql,params);
         System.out.println(sql);
     }
+    /**
+     * test merge
+     * */
+    @Test
+    public void testMerge(){
+        Configuration configuration = new Configuration().configure();
+        System.out.println("================Hibernate session配置完成,开始操作数据库============");
+        System.out.println("================开始创建ServiceRegistry================");
+        /**
+         * ServiceRegistry sr = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+         */
+        System.out.println("================开始创建session================");
+        SessionFactory sf = configuration.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+        User user0 = new User("1","xxx","123456");
+        User user1 = new User("1","xxd","123456");
+        /**
+         * 制造merge预发条件,ID冲突
+         * 会报一个a different object with the same identifier value was already associated with the session: [com.work.hn.model.User#1]异常
+         * */
+        session.save(user0);
+//        session.save(user1);
+        /**
+         * 进行merge,对user1进行merge
+         * */
+        User oldUser = (User)session.merge(user1);
+        System.out.println(oldUser);
+        tx.commit();
+        session.close();
+    }
 }
